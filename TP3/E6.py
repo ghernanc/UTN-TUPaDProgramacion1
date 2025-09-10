d870ccf31241245258fde1756a99f6d0c084e845
ghernanc <168318032+ghernanc@users.noreply.github.com>
2025-09-10 14:34:59 -0300

    Delete TP3 directory

--- TP3/E6.py
+++
@@ -1,5 +0,0 @@
-#Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
-#entre 0 y 100, en orden decreciente
-
-for numero in range(100, -1, -2): # Incluyo en 0 (con el -1),que tambien es par
-    print(numero)                 #El 100 es el valor inicial y el -2 es el paso, osea que va decreciendo de 2 en 2. 
